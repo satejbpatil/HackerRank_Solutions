8a4fa1f212bfc601b79e7cf42a2ccf865e1b35d2
Satej Balaso Patil <112341637+satejbpatil@users.noreply.github.com>
2023-01-02 23:33:16 +0530

    Prepare>Algorithms>Implementation>Picking Numbers

---
+++ picking_numbers.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int pickingNumbers(vector<int> a) {
+    int curr_len = 0, max_len = 0;
+
+    sort(a.begin(), a.end());
+    
+    int min_el = a[0], curr_el, next_el;
+    
+    for (int idx = 0; idx < a.size(); idx++) {
+        curr_el = a[idx];
+        next_el = a[idx+1];
+        
+        if (next_el - curr_el <= 1 && next_el - min_el <= 1) {
+            curr_len++;
+        } else {
+            curr_len = 0;
+            min_el = next_el;
+        }
+        
+        if (curr_len >= max_len) max_len = curr_len;
+        
+    }
+    
+    return max_len + 1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = pickingNumbers(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
