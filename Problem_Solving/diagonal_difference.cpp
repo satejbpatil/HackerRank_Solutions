0468100a07e817bfcdfd3053b17055e07ce47e70
Satej Balaso Patil <112341637+satejbpatil@users.noreply.github.com>
2023-01-05 01:04:58 +0530

    Create diagonal_difference.cpp

---
+++ Problem_Solving/diagonal_difference.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n ;
+    int sum1=0;
+    int sum2=0;
+    cin>>n;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i==j){
+                sum1=sum1+a[i][j];
+            }
+            if ((i + j) == (n - 1)){
+                sum2=sum2+a[i][j];
+            }
+        }
+    }
+    int ans=sum1-sum2;
+    ans = abs(ans);
+    cout<<ans;
+    return 0;
+}
